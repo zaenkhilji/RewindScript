cb546cec964c163337cf6ce6f665537a12b5c612
Zaen Khilji <zaen.khilji@gmail.com>
2017-06-11 23:49:35 +0200

    Add files via upload

---
+++ source/net/skycord/Service.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2017, SkyCord AS
+ *
+ * Permission is hereby denied, this is software is private and should not be distributed further, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so, subject to conditions.
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+ * of the Software.
+ */
+package net.skycord;
+
+
+/**
+ * @author Zaen Khilji
+ */
+public class Service {
+
+}
